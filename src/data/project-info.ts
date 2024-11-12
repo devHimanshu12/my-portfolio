1fcf001b03934a852e89e14f8fa66e89e3d634fe
himanshu <shimanshu94@gmail.com>
2024-11-12 22:34:38 +0530

    adde more projects

--- src/data/project-info.ts
+++ src/data/project-info.ts
@@ -3,14 +3,14 @@ export const ProjectInfo = [
         label:"Private Circle",
         link:"https://privatecircle.co/",
         description:"Developed company profile which show private companies data in the form of tabular, graph and cards. Also created ui tool kit library which reduced development time and error resolving time reduced. Created dynmaic component which could handle more then 50+ panels just by sending different configuration",
-        techStack:['Angular','TypeScript','HTML','CSS',]
+        techStack:['Angular','TypeScript','HTML5','CSS3','Rxjs','Tailwind','SonarQube','Angular Material','Jasmine','Karma','GitHub','Git','Docker']
     },
     {
         label:"Cloud Defense",
         link:"https://www.clouddefense.ai/",
         role:"Software Engineer",
         description:"Created Dashboard which shows information of application and cloud of users company and all these information were shown in the form of Graphs for which i created data visualization library using Highcharts which  could handle basic and advanced charts.",
-        techStack:['Angular','TypeScript','HTML','CSS',]
+        techStack:['Angular','TypeScript','HTML','CSS','Angular Material']
     },{
         label:"Source Ease",
         link:"https://www.sourceease.in/",
@@ -18,9 +18,15 @@ export const ProjectInfo = [
         techStack:['Angular','TypeScript','HTML','CSS',]
     },
     {
-        label:"Note taking app",
+        label:"https://devhimanshu12.github.io/note-taking-app/",
         link:"https://www.sourceease.in/",
-        description:"A note taking app where you can create tags and use it while creating notes from dropdown and it will saved in your system",
-        techStack:['React','TypeScript','HTML','CSS',]
+        description:"A note taking app where you can create tags and use it while creating notes from dropdown and it will saved in your system and used react with typescript.",
+        techStack:['React','TypeScript','HTML','CSS','Redux','Redux toolkit','GIT']
+    },
+    {
+        label:"Investment Calculator",
+        link:"https://devhimanshu12.github.io/calculate-investment/",
+        description:"Calulate return yearly and see the difference just by entering the input values and this i build on react js and learn core concept of react.",
+        techStack:['React','TypeScript','HTML','CSS','Redux','Redux toolkit','GIT']
     }
 ]
